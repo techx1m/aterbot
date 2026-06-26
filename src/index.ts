18f039b871d828f9da991b2f8315b01843f6f444
techx1m <deepadevi2058@gmail.com>
2026-06-26 15:27:28 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import http from 'http';
+
+// Railway Health Check Bypass Code
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot running 24/7!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Web server listening on port ${PORT}`);
+});
 import initBot from "./bot.ts";
 import initWeb from "./web.ts";
 
